4f0b21a8b7f382d7b7c4a8a28820401be5594590
Nathan <61805267+Nathan-925@users.noreply.github.com>
2023-01-17 17:02:56 -0500

    Added Math.h

---
+++ Math.cpp
@@ -0,0 +1,15 @@
+/*
+ * Math.cpp
+ *
+ *  Created on: Jan 3, 2023
+ *      Author: Nathan
+ */
+#include "Math.h"
+
+namespace priori{
+
+	Point::Point() : x(), y() {};
+
+	Point::Point(int x, int y) : x(x), y(y) {};
+
+}
