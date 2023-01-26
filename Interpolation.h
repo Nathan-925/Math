80ce33c4afccb8b8936812eb216e66e6ee6d1110
Nathan <nathaniel.dunlap925@gmail.com>
2023-01-26 12:00:32 -0500

    lerp now returns a forward_list

--- Interpolation.h
+++ Interpolation.h
@@ -9,20 +9,20 @@
 #define INTERPOLATION_H_
 
 #include <cmath>
+#include <forward_list>
 
 namespace priori{
 	template <class T>
-	T* lerp(int i0, T d0, int i1, T d1){
-		int size = abs(i1-i0);
-		T* arr = new T[size+1];
-		T slope = (d1-d0)/(i1-i0);
-		T val = d0;
-		for(int i = 0; i < size; i++){
-			arr[i] = val;
+	std::forward_list<T> lerp(int i0, T d0, int i1, T d1){
+		std::forward_list<T> list;
+		T slope = (d0-d1)/(i0-i1);
+		T val = d1;
+		for(int i = 0; i < i1-i0; i++){
+			list.push_front(val);
 			val += slope;
 		}
-		arr[size] = d1;
-		return arr;
+		list.push_front(d0);
+		return list;
 	}
 
 	template <class T>
