49842c7cf4e807268402233a82ea0abb8b6713f7
Nathan <nathaniel.dunlap925@gmail.com>
2023-03-31 20:58:04 -0400

    Fixed operators

--- Geometry.cpp
+++ Geometry.cpp
@@ -12,15 +12,15 @@ namespace priori{
 
 	Vector::Vector(double x, double y) : x(x), y(y) {};
 
-	Vector Vector::operator+(const Vector &other){
+	Vector Vector::operator+(const Vector &other) const{
 		return Vector(x+other.x, y+other.y);
 	}
 
-	Vector Vector::operator-(const Vector &other){
+	Vector Vector::operator-(const Vector &other) const{
 		return Vector(x-other.x, y-other.y);
 	}
 
-	double Vector::operator*(const Vector &other){
+	double Vector::operator*(const Vector &other) const{
 		return x*x + y*y;
 	}
 
@@ -34,11 +34,11 @@ namespace priori{
 		return *this;
 	}
 
-	Vector Vector::operator*(const double &d){
+	Vector Vector::operator*(const double &d) const{
 		return Vector(x*d, y*d);
 	}
 
-	Vector Vector::operator/(const double &d){
+	Vector Vector::operator/(const double &d) const{
 		return Vector(x/d, y/d);
 	}
 
@@ -52,15 +52,62 @@ namespace priori{
 		return *this;
 	}
 
-	bool Vector::operator==(const Vector &other){
+	bool Vector::operator==(const Vector &other) const{
 		return x == other.x && y == other.y;
 	}
 
-	bool Vector::operator!=(const Vector &other){
+	bool Vector::operator!=(const Vector &other) const{
 		return x != other.x && y != other.y;
 	}
 
 	Point::Point() : Vector() {};
+
 	Point::Point(double x, double y) : Vector(x, y) {};
+
+	Point::Point(Vector other) : Vector(other) {};
+
+	Point Point::operator+(const Point &other) const{
+		return Vector::operator+(other);
+	}
+
+	Point Point::operator-(const Point &other) const{
+		return Vector::operator-(other);
+	}
+
+	double Point::operator*(const Point &other) const{
+		return Vector::operator*(other);
+	}
+
+	Point Point::operator+=(const Point &other){
+		return Vector::operator+=(other);
+	}
+
+	Point Point::operator-=(const Point &other){
+		return Vector::operator-=(other);
+	}
+
+	Point Point::operator*(const double &d) const{
+		return Vector::operator*(d);
+	}
+
+	Point Point::operator/(const double &d) const{
+		return Vector::operator/(d);
+	}
+
+	Point Point::operator*=(const double &d){
+		return Vector::operator*=(d);
+	}
+
+	Point Point::operator/=(const double &d){
+		return Vector::operator/=(d);
+	}
+
+	bool Point::operator==(const Point &other) const{
+		return Vector::operator==(other);
+	}
+
+	bool Point::operator!=(const Point &other) const{
+		return Vector::operator!=(other);
+	}
 
 }
--- Geometry.h
+++ Geometry.h
@@ -12,33 +12,53 @@
 
 namespace priori{
 
+	struct Point;
+
 	struct Vector{
 		double x, y;
 
 		Vector();
 		Vector(double x, double y);
 
-		Vector operator+(const Vector &other);
-		Vector operator-(const Vector &other);
+		Vector operator+(const Vector &other) const;
+		Vector operator-(const Vector &other) const;
 
-		double operator*(const Vector &other);
+		double operator*(const Vector &other) const;
 
 		Vector operator+=(const Vector &other);
 		Vector operator-=(const Vector &other);
 
-		Vector operator*(const double &d);
-		Vector operator/(const double &d);
+		Vector operator*(const double &d) const;
+		Vector operator/(const double &d) const;
 
 		Vector operator*=(const double &d);
 		Vector operator/=(const double &d);
 
-		bool operator==(const Vector &other);
-		bool operator!=(const Vector &other);
+		bool operator==(const Vector &other) const;
+		bool operator!=(const Vector &other) const;
 	};
 
 	struct Point : public Vector{
 		Point();
 		Point(double x, double y);
+		Point(Vector other);
+
+		Point operator+(const Point &other) const;
+		Point operator-(const Point &other) const;
+
+		double operator*(const Point &other) const;
+
+		Point operator+=(const Point &other);
+		Point operator-=(const Point &other);
+
+		Point operator*(const double &d) const;
+		Point operator/(const double &d) const;
+
+		Point operator*=(const double &d);
+		Point operator/=(const double &d);
+
+		bool operator==(const Point &other) const;
+		bool operator!=(const Point &other) const;
 	};
 
 	typedef std::forward_list<Point> Polygon;
